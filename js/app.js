1fcef194d253d60f8e74e3d7b2f04d5031f2a65e
katherinelef <katherine.lef@hotmail.com>
2018-02-14 17:04:37 -0500

    añadiendo funcionalidad

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const btn = document.getElementById('search');
+const responseContainer = document.getElementById('response-container');
+
+btn.addEventListener('click',function () {
+   for(i=1; i<20; i++)
+getPeople() 
+});
+
+function getPeople() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET',`https://swapi.co/api/people/${i}`);
+  articleRequest.onload = addPeople;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+    }
+
+function handleError(){
+  console.log('se ha presentado un error');
+}
+
+function addPeople(){
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+  let names = data.name;
+  let birthday = data.birth_year;
+  let hair = data.hair_color;
+  let skin = data.skin_color; 
+  let gender = data.gender; 
+
+  let name = document.createElement('ul');
+  let liTitle = document.createElement('li');
+  let birth = document.createElement('li');
+  let hairColor = document.createElement('li');
+  let skinColor = document.createElement('li');
+  let TheGender = document.createElement('li');
+
+  liTitle.innerText = names;
+  birth.innerText = birthday;
+  hairColor.innerText = hair;
+  skinColor.innerText = skin;
+  TheGender.innerText = gender;
+
+  name.appendChild(liTitle);
+  name.appendChild(birth);
+  name.appendChild(hairColor);
+  name.appendChild(skinColor);
+  name.appendChild(TheGender);
+  responseContainer.appendChild(name);
+
+
+}
